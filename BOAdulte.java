22459dee6d736647f2b6940af142911fa45bf620
shikagin <125314835+shikagin@users.noreply.github.com>
2024-05-08 21:35:43 +0000

    adding boadulte et boenfant

--- BOAdulte.java
+++ BOAdulte.java
@@ -1,6 +1,37 @@
 
 public class BOAdulte extends BO {
 	
-	
 
+
+	private String diplome;
+	private String profession;
+	private String numTel;
+	
+	public String getDiplome()
+	{
+		return(this.diplome);
+	}
+	public String getProfission()
+	{
+		return(this.profession);
+	}
+	public String getNumTel()
+	{
+		return(this.numTel);
+	}
+	public void setNumTel(String numTel)
+	{
+		this.numTel=numTel;
+	}
+	public void setDiplome(String diplome)
+	{
+		this.diplome=diplome;
+	}
+	public void setProfission(String profission)
+	{
+		this.profession =profission;
+	}
+	
 }
+
+
--- BOEnfant.java
+++ BOEnfant.java
@@ -1,4 +1,22 @@
 
 public class BOEnfant extends BO {
-
+private String classeEtude;
+	private String numTel;
+	
+	public String getClasseEtude()
+	{
+		return(this.classeEtude);
+	}
+	public void setClasseEtude(String classeEtude)
+	{
+		this.classeEtude=classeEtude;
+	}
+	public String getNumTel()
+	{
+		return(this.numTel);
+	}
+	public void setNumTel(String numTel)
+	{
+		this.numTel=numTel;
+	}
 }
